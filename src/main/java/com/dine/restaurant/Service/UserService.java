59e82721bf7cbc8e1b79fb7fc16a4c16742b6ada
unknown <janhavi.patil245@gmail.com>
2022-11-22 21:21:05 +0530

    Added code for post request

--- src/main/java/com/dine/restaurant/Service/UserService.java
+++ src/main/java/com/dine/restaurant/Service/UserService.java
@@ -1,9 +1,11 @@
 package com.dine.restaurant.Service;
 
+import com.dine.restaurant.DocumentModel.Users;
 
 public interface UserService {
 	public void addSampleData();
 	public void listAll();
 	public void findFirst();
 	public void findByRegex();
+	public void saveOrUpdateStudent(Users user);
 }
