9a9015eb02330cabf3f69a1830b3e828057edf4b
Nikhil <nikhil.sabe@impactsure.com-at-709638873316>
2023-01-21 21:47:10 +0530

    login page custom design

--- Oauth-authorization-server/src/main/java/com/dine/oauthserver/config/DefaultSecurityConfig.java
+++ Oauth-authorization-server/src/main/java/com/dine/oauthserver/config/DefaultSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.dine.oauthserver.service.CustomAuthenticationProvider;
@@ -29,5 +30,12 @@ public class DefaultSecurityConfig {
     public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
         authenticationManagerBuilder
                 .authenticationProvider(customAuthenticationProvider);
+    }
+    
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.debug(false)
+          .ignoring()
+          .antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
     }
 }
